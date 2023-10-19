b645fd6f7adadc7febcaa3faf20dad409c8eb404
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-10-19 11:48:22 +0400

    feat: add modal-window

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,18 @@ import { useEffect, useState } from 'react'
 import Chat from './components/chat';
 import TagsFilter from './components/tagsFilter';
 import { findAllTags } from './shared/findAllTags';
+import ModalWindow from './components/modalWindow';
 
 function App() {
-  const [messagesReceived, setMessagesReceived] = useState<string[]>([])
+  const [messagesReceived, setMessagesReceived] = useState<string[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const baseURL = import.meta.env.VITE_REACT_APP_BACKEND_URL;
   const socket = io(baseURL)
   const [tags, setTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    setIsModalOpen(true)
+  }, [])
 
   const receiveMessageHandler = (data: { message: string; }) => {
     const dataTags = findAllTags(data.message)
@@ -35,13 +41,16 @@ function App() {
   }, [socket]);
 
   return (
-    <div className='App d-flex flex-column border border-black w-75'>
-      <div className='d-flex flex-row' style={{height: '93vh'}}>
-        <TagsFilter socket={socket} tags={tags} setTags={setTags} />
-        <Chat messagesReceived={messagesReceived} socket={socket} />
+    <>
+      <ModalWindow show={isModalOpen} handleClose={() => setIsModalOpen(false)} />
+      <div className='App d-flex flex-column border border-black w-75'>
+        <div className='d-flex flex-row' style={{height: '93vh'}}>
+          <TagsFilter socket={socket} tags={tags} setTags={setTags} />
+          <Chat messagesReceived={messagesReceived} socket={socket} />
+        </div>
+        <h4 className='mt-4'>You can write whatever you want and it will be here forever!</h4>
       </div>
-      <h4 className='mt-4'>You can write whatever you want and it will be here forever!</h4>
-    </div>
+    </>
   )
 }
 
